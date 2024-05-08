e07df5dbaa2d9377f482d351da460afc3c8d8356
billfang01 <165020916+billfang01@users.noreply.github.com>
2024-05-09 00:04:31 +0800

    Update schooltestmode.py

--- schooltestmode.py
+++ schooltestmode.py
@@ -8,11 +8,11 @@ Created on Tue May  7 22:01:42 2024
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from streamlit_option_menu import option_menu
+#from streamlit_option_menu import option_menu
 from numerize.numerize import numerize
 import time
-from streamlit_extras.metric_cards import style_metric_cards
-st.set_option('deprecation.showPyplotGlobalUse', False)
+#from streamlit_extras.metric_cards import style_metric_cards
+#st.set_option('deprecation.showPyplotGlobalUse', False)
 import plotly.graph_objs as go
 import matplotlib.pyplot as plt
 plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei']
