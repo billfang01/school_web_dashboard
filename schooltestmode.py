c355cd5eacd286077252cc41d43d472f29eb05a7
billfang01 <165020916+billfang01@users.noreply.github.com>
2024-05-09 00:32:16 +0800

    Update schooltestmode.py

--- schooltestmode.py
+++ schooltestmode.py
@@ -8,7 +8,7 @@ Created on Tue May  7 22:01:42 2024
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-#from streamlit_option_menu import option_menu
+from streamlit_option_menu import option_menu
 from numerize.numerize import numerize
 import time
 from streamlit_extras.metric_cards import style_metric_cards
